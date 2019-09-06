5256017dbcf84567f96a1aa3227c5ab7cb62b686
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-06 17:28:53 +0000

    Automatically backed up by Learn

--- db/migrate/03_add_season_and_attribute.rb
+++ db/migrate/03_add_season_and_attribute.rb
@@ -1,6 +1,6 @@
 class AddSeasonAndAttribute < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :season, :string
-    add_column :shows, :attribute, :boolean
+    add_column :shows, :attribute, :boolean, default: true
   end
 end
