d347c97214934c8e96c9823f7379b9a7e695aee0
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-06 17:09:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.find_by(rating: self.highest_rating)
   end
+
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
+  end
 end
